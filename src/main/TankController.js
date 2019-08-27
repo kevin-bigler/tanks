95e3215bc053d8cda6ec5ad003077b5aaed09192
Kevin Bigler <kevinbigler@gmail.com>
2019-08-27 22:04:12 +0100

    add note about the tank actions map etc

--- src/main/TankController.js
+++ src/main/TankController.js
@@ -29,7 +29,8 @@ export default class TankController {
         // queue command that corresponds to key+action, passing {key, action, event, pressed}
         keysPressed.entries().forEach((key, keyPress) => {
             const keyName = keyMap[key];
-            const actions = getActions(keyName); // TODO: getActions should take a key name (see keyMap) and return a set of actions. maybe pub-sub per key name? :D
+            const actions = getActions(keyName); // TODO: getActions should take a key name (see keyMap) and return a set of actions (action names, eg MOVE_DOWN, etc). maybe pub-sub per key name? :D
+            // TODO: also create a map of action names to actions (functions or command objects with a "run" function, or w/e)
             if (actions.length > 0) {
                 console.log('queueing actions for key [' + key + ']');
                 this.queue.concat(actions);
